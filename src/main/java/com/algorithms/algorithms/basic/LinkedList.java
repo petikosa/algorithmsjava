26eaa78a51e1e7254595fd0e7f285f014b1f2d36
petikosa <petikosa@yahoo.com>
2025-06-01 12:48:18 +0200

    LinkedList added

--- src/main/java/com/algorithms/algorithms/basic/LinkedList.java
+++ src/main/java/com/algorithms/algorithms/basic/LinkedList.java
@@ -1,5 +1,7 @@
 package com.algorithms.algorithms.basic;
 
+import java.util.HashSet;
+
 public class LinkedList {
 
     public static class Node {
@@ -29,5 +31,30 @@ public class LinkedList {
             curr = front;
         }
         return prev;
+    }
+
+    public static boolean findCycle(Node head) {
+        HashSet<Node> processedNodes = new HashSet<>();
+        while (head != null) {
+            if (processedNodes.contains(head)) {
+                return true;
+            }
+            processedNodes.add(head);
+            head = head.next;
+        }
+        return false;
+    }
+
+    public static boolean findCycleFloyd(Node head) {
+        Node slow = head;
+        Node fast = head;
+        while (slow != null && fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/test/java/com/algorithms/algorithms/LinkedListTests.java
+++ src/test/java/com/algorithms/algorithms/LinkedListTests.java
@@ -24,6 +24,24 @@ public class LinkedListTests {
         assertThat(printLinkedList(result)).isEqualTo("4 3 2 1");
     }
 
+    @Test
+    void findCycles() {
+        // given
+        LinkedList.Node head = new LinkedList.Node(1);
+        head.next = new LinkedList.Node(2);
+        head.next.next = new LinkedList.Node(3);
+        head.next.next.next = new LinkedList.Node(4);
+        head.next.next.next.next = head.next;
+
+        // when
+        boolean result = LinkedList.findCycle(head);
+        boolean resultLoyd = LinkedList.findCycleFloyd(head);
+
+        //then
+        assertThat(result).isTrue();
+        assertThat(resultLoyd).isTrue();
+    }
+
     private String printLinkedList(LinkedList.Node head) {
         LinkedList.Node temp = head;
         StringBuilder s = new StringBuilder();
