6afa3f87aff6ad369aa0a11f83eca6e22bc9cad1
petikosa <petikosa@yahoo.com>
2025-06-04 20:30:08 +0200

    Binary search tree added

--- src/main/java/com/algorithms/algorithms/basic/Trees.java
+++ src/main/java/com/algorithms/algorithms/basic/Trees.java
@@ -6,7 +6,7 @@ import java.util.Queue;
 public class Trees {
 
     public static class Node {
-        int data;
+        public int data;
         public Node left;
         public Node right;
 
@@ -40,5 +40,46 @@ public class Trees {
         }
 
         return depth;
+    }
+
+    public static void mirror(Node root) {
+        if (root == null) {
+            return;
+        }
+        mirror(root.left);
+        mirror(root.right);
+
+        Node temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+    }
+
+    public static boolean isIdentical(Node root1, Node root2) {
+        if (root1 == null && root2 == null) {
+            return true;
+        } else if (root1 != null && root2 == null) {
+            return false;
+        } else if (root1 == null) {
+            return false;
+        } else {
+            return root1.data == root2.data &&
+                    isIdentical(root1.left, root2.left) &&
+                    isIdentical(root1.right, root2.right);
+        }
+    }
+
+    public static Node lowestCommonAncestor(Node root, Node firstNode, Node secondNode) {
+        while (root != null) {
+            if (root.data < Math.min(firstNode.data, secondNode.data)) {
+                root = root.right;
+            }
+            else if (root.data > Math.max(firstNode.data, secondNode.data)) {
+                root = root.left;
+            }
+            else {
+                return root;
+            }
+        }
+        return null;
     }
 }
--- src/test/java/com/algorithms/algorithms/TreesTests.java
+++ src/test/java/com/algorithms/algorithms/TreesTests.java
@@ -24,4 +24,62 @@ public class TreesTests {
         // then
         assertThat(depth).isEqualTo(3);
     }
+
+    @Test
+    void testMirroredTree() {
+        // given
+        Trees.Node node = new Trees.Node(1);
+        node.left = new Trees.Node(3);
+        node.right = new Trees.Node(2);
+        node.right.right = new Trees.Node(5);
+        node.left.left = new Trees.Node(8);
+
+        Trees.Node mirrorNode = new Trees.Node(1);
+        mirrorNode.left = new Trees.Node(2);
+        mirrorNode.right = new Trees.Node(3);
+        mirrorNode.right.right = new Trees.Node(8);
+        mirrorNode.left.left = new Trees.Node(5);
+
+        // when
+        Trees.mirror(node);
+
+        // then
+        assertThat(Trees.isIdentical(node, mirrorNode)).isTrue();
+    }
+
+    @Test
+    void testLowestCommonAncestor() {
+        // given
+        Trees.Node node = new Trees.Node(8);
+        node.left = new Trees.Node(4);
+        node.right = new Trees.Node(12);
+        Trees.Node firstNode = new Trees.Node(14);
+        Trees.Node secondNode = new Trees.Node(10);
+        node.right.right = firstNode;
+        node.right.left = secondNode;
+
+        // when
+        Trees.Node resultNode = Trees.lowestCommonAncestor(node, firstNode, secondNode);
+
+        // then
+        assertThat(resultNode.data).isEqualTo(12);
+    }
+
+    @Test
+    void testLowestCommonAncestorHigherUp() {
+        // given
+        Trees.Node node = new Trees.Node(8);
+        node.left = new Trees.Node(4);
+        node.right = new Trees.Node(12);
+        Trees.Node firstNode = new Trees.Node(14);
+        Trees.Node secondNode = new Trees.Node(4);
+        node.right.right = firstNode;
+        node.right.left = secondNode;
+
+        // when
+        Trees.Node resultNode = Trees.lowestCommonAncestor(node, firstNode, secondNode);
+
+        // then
+        assertThat(resultNode.data).isEqualTo(8);
+    }
 }
